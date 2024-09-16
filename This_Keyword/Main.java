75d729a76e59fe2c2af0a473a355fc5698e3e8c9
Khorsed Alam <khorsedalam0472@gmail.com>
2024-09-16 16:17:35 +0600

    This Keyword

--- This_Keyword/Main.java
+++ This_Keyword/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Person p1= new Person("Khorsed",20);
         p1.Display();
+
+        Person p2= new Person("Khorsed",39,"black");
+       p2.Display();
     }
 }
--- This_Keyword/Person.java
+++ This_Keyword/Person.java
@@ -3,12 +3,19 @@ package This_Keyword;
 public class Person {
     String name;
     int age;
+    String haircolor;
     Person (String name, int age){
         this.name=name;
         this.age= age;
     }
+    Person(String name , int age, String haircolor){
+        this(name,age);
+        this.haircolor= haircolor;
+    }
     void Display(){
         System.out.println(name);
         System.out.println(age);
+        System.out.println(haircolor);
+        System.out.println();
     }
 }

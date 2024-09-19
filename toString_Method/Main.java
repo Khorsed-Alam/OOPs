91881498df50f01e4518c234f7a37fbbb354437f
Khorsed Alam <khorsedalam0472@gmail.com>
2024-09-20 02:43:19 +0600

    toString Method

---
+++ toString_Method/Main.java
@@ -0,0 +1,12 @@
+package toString_Method;
+
+public class Main {
+    public static void main(String[] args) {
+        Person P1= new Person("Khorsed",20);
+
+        Person P2= new Person("Khorsed Alam",21);
+        System.out.println(P1);
+        System.out.println(P2);
+
+    }
+}
---
+++ toString_Method/Person.java
@@ -0,0 +1,14 @@
+package toString_Method;
+
+public class Person {
+    String name;
+    int age;
+    Person(String name , int age){
+        this.name= name;
+        this.age=age;
+    }
+    public String toString(){
+        return "Name: "+name+ "\n Age: "+age;
+    }
+
+}
